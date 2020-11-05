cac6e3e6a690f5f95ef833679874b734b8e4bd7b
a.kardash <sash.kardash@gmail.com>
2020-11-05 12:05:25 +0300

    updated apk

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 from selenium import webdriver
 import time
-import urllib
 
-urllib.urlretrieve ("https://dw.uptodown.com/dwn/8c_0a6VR8EofzKPe6qvRMtzyAaECK4AHZOC7ZJw1djMYNqzTlmr6qNNKDVy66wx15CV2meHMNtvJ_CxQUKOEi9QrsLrC497Anh6Ye8LbupS_mkrD_iUw-lpVD3LghYXk/43enMmaHRurwOYGt_SMXOeULadzrFMx4tQQ_-3xuUuxwkSJ3jFP4Kx2tfUX3m0CS7D522bpnyaxBoOfS51gznHtKNs0w7eCzmqZhOEt5Bx72uWBL6ottns7Aotyw3ezd/LiiQjy0mrTdypm26ePpKQDRbdFuSijql_1QCTDRMOzlcxJ2eS_c8Y2pi3HYTBVJFkAuV0kG_7udB_o5lkG_jfAedxA-0sK6KNWV8jUmvb4Q=/", "app.apk")
 
 capabilities = {
     "browserName": "android",
     "version": "9.0",
-    'app': 'app.apk',
+    'app': 'https://github.com/sozdai/behave-sample/raw/main/org.wikipedia.com.apk',
     "selenoid:options": {
         "enableVNC": True,
         "enableVideo": False
