2bd80f9d4e9541c746fc0997686c82204a9f921d
sozdai <sash.kardash@gmail.com>
2020-11-09 07:33:46 +0300

    removed lint

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
-from appium import webdriver
+from selenium import webdriver
 import time
 
+# capabilities = {
+#     "deviceName": "android",
+#     "version": "9.0",
+#     'app': 'https://github.com/sozdai/behave-sample/raw/main/com.instagram.android.apk',
+#     'appActivity': 'com.instagram.mainactivity.LauncherActivity',
+#     'appPackage': 'com.instagram.android',
+#     "selenoid:options": {
+#         "enableVNC": True,
+#         "enableVideo": False
+#     }
+# }
+#
+# driver = webdriver.Remote(
+#     command_executor="http://localhost:4444/wd/hub",
+#     desired_capabilities=capabilities)
 
-capabilities = {
-    "deviceName": "android",
-    "version": "9.0",
-    'app': 'https://github.com/sozdai/behave-sample/raw/main/com.instagram.android.apk',
-    'appActivity': 'com.instagram.mainactivity.LauncherActivity',
-    'appPackage': 'com.instagram.android',
-    "selenoid:options": {
-        "enableVNC": True,
-        "enableVideo": False
-    }
-}
-
-driver = webdriver.Remote(
-    command_executor="http://localhost:4444/wd/hub",
-    desired_capabilities=capabilities)
-
+driver = webdriver.Chrome()
 
 time.sleep(10)
-time.sleep(10)
+driver.get("https://www.google.com")
 
 driver.quit()
