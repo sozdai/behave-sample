8700c5a3f2399d05fb89ff3cdcbfa5cee268a5f3
sozdai <sash.kardash@gmail.com>
2020-11-09 08:38:20 +0300

    updated to ubuntu

--- test.py
+++ test.py
@@ -19,9 +19,7 @@ import os
 #     desired_capabilities=capabilities)
 from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome()
 
 driver.get("https://www.google.com")
 
